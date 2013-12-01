89f23bec14bb4ed474bbf81acb90262d2517568a
Benoit Gantaume <benoit@agilidee.com>
2013-12-01 20:40:47 +0100

    Implemented

--- lib/full_name.rb
+++ lib/full_name.rb
@@ -1,6 +1,29 @@
-class FullName
-  def self.hi
-    puts 'Hello World'  
+module FullName
+  def full_name locale = :en
+    if locale != :en and locale != :fr
+      return full_name(:en)
+    end
+
+    if first_name and first_name.size > 0
+      if last_name and last_name.size > 0
+        case locale  
+          when :en then
+            full_name = [first_name.capitalize, last_name.capitalize].join(' ')
+          when :fr then
+            full_name = "#{last_name.upcase}, #{first_name.capitalize}"
+        end
+      
+      else
+        full_name = first_name.capitalize
+      end
+    else
+      case locale  
+        when :en then
+          full_name = last_name.capitalize
+        when :fr then
+          full_name = last_name.upcase           
+      end
+    end
   end
 end
 
--- spec/lib/full_name_spec.rb
+++ spec/lib/full_name_spec.rb
@@ -1,7 +1,42 @@
 require 'spec_helper.rb'
 
 describe FullName do
-  it 'should fail' do
-    true.should be_false
+  include FullName
+  
+  [['Benoit', 'Gantaume', :en, 'Benoit Gantaume'],
+   ['benoit', 'gantaume', :en, 'Benoit Gantaume'],
+   ['Benoit', '', :en, 'Benoit'],
+   ['benoit', '', :en, 'Benoit'],
+   ['Benoit', nil, :en, 'Benoit'],
+   ['', 'Gantaume', :en, 'Gantaume'],
+   ['', 'gantaume', :en, 'Gantaume'],
+   [nil, 'Gantaume', :en, 'Gantaume'],
+
+   ['', 'gantaume', :fr, 'GANTAUME'],
+   ['Benoit', 'Gantaume', :fr, 'GANTAUME, Benoit'],
+   ['benoit', 'gantaume', :fr, 'GANTAUME, Benoit'],
+   ['benoit', '', :fr, 'Benoit'],
+   ].each do |params|
+    context 'parametric' do
+      let(:first_name) { params[0] }
+      let(:last_name) { params[1] }
+      it 'aggregates both' do
+        expect(full_name(params[2])).to eq(params[3])
+      end
+    end
   end
+
+  context 'using locales' do
+    let(:first_name) { 'Benoit' }
+    let(:last_name) { 'Gantaume' }
+    
+    it 'returns :en locale by default' do
+      expect(full_name).to eq(full_name(:en))
+    end
+
+    it 'returns :en when using unknown locale' do
+      expect(full_name(:unknown)).to eq(full_name(:en))
+    end
+  end
+
 end
