f47bb1ddb94cb236fc71b9b75b8030de6a8b1703
Benoit Gantaume <benoit@agilidee.com>
2013-12-04 14:58:57 +0100

    Refactored code

--- lib/full_name.rb
+++ lib/full_name.rb
@@ -1,29 +1,42 @@
 module FullName
-  def full_name locale = :en
-    if locale != :en and locale != :fr
-      return full_name(:en)
-    end
+  LOCALES = [:en, :fr]
 
-    if first_name and first_name.size > 0
-      if last_name and last_name.size > 0
-        case locale  
-          when :en then
-            full_name = [first_name.capitalize, last_name.capitalize].join(' ')
-          when :fr then
-            full_name = "#{last_name.upcase}, #{first_name.capitalize}"
-        end
-      
+  def full_name locale = :en
+    return full_name(:en) unless LOCALES.include? locale
+
+    unless first_name.nil? or first_name.empty?
+      unless last_name.nil? or last_name.empty?
+        build_with_both locale     
       else
-        full_name = first_name.capitalize
+        build_with_firstname locale     
       end
     else
-      case locale  
-        when :en then
-          full_name = last_name.capitalize
-        when :fr then
-          full_name = last_name.upcase           
-      end
+      build_with_lastname locale     
     end
   end
+
+  private 
+    def build_with_both locale
+      case locale  
+        when :en then
+          [first_name.capitalize, last_name.capitalize].join(' ')
+        when :fr then
+          "#{last_name.upcase}, #{first_name.capitalize}"
+      end
+    end
+    
+    def build_with_firstname locale
+      full_name = first_name.capitalize
+    end
+
+    def build_with_lastname locale
+      case locale  
+        when :en then
+          last_name.capitalize
+        when :fr then
+          last_name.upcase           
+      end
+    end
+
 end
 
